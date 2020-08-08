eb3dfe9bfabe7e246b86b778cd4c89c785e33d63
Djendra Priono <djendrapriono@hotmail.com>
2020-08-08 19:03:32 +0700

    typo penulisan

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('role', ['administrator', 'teacher', 'student'])->defaukt('student');
+            $table->enum('role', ['administrator', 'teacher', 'student'])->default('student');
             $table->tinyInteger('status')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
