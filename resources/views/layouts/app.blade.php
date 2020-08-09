f993ddc3ecb6f47f6af82a62f09d9554b133ff05
Djendra Priono <djendrapriono@hotmail.com>
2020-08-09 12:08:15 +0700

    menyesuaikan layout dengan halaman admin teacher

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Shards Dashboard Lite - Free Bootstrap Admin Template – DesignRevision</title>
+    <title>E-Learning</title>
     <meta name="description"
         content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{csrf_token()}}">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
@@ -16,6 +17,7 @@
         href="{{ url('assets/styles/shards-dashboards.1.1.0.min.css') }}">
     <link rel="stylesheet" href="{{ url('assets/styles/extras.1.1.0.min.css') }}">
     <script async defer src="https://buttons.github.io/buttons.js"></script>
+    @stack('styles')
 </head>
 
 <body class="h-100">
@@ -26,9 +28,9 @@
                     <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
                         <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
                             <div class="d-table m-auto">
-                                <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;"
-                                src="{{ url('assets/images/shards-dashboards-logo.svg') }}" alt="Shards Dashboard">
-                                <span class="d-none d-md-inline ml-1">Shards Dashboard</span>
+                                <i id="main-logo" class="material-icons d-inline-block align-top mr-1"
+                                    style="max-width: 25px;" alt="Shards Dashboard">
+                                    school</i><span class="d-none d-md-inline ml-1">E-Learning</span>
                             </div>
                         </a>
                         <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
@@ -45,54 +47,22 @@
             <main class="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
                 <!-- Main Navbar -->
                 <div class="main-navbar sticky-top bg-white">
-                    @yield('layouts.navbar')
+                    @yield('navbar')
                 </div>
                 <!-- / .main-navbar -->
-                <div class="alert alert-success alert-dismissible fade show mb-0" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                    <i class="fa fa-check mx-2"></i>
-                    <strong>Success!</strong> Your profile has been updated! </div>
+                @include('layouts.message')
                 <div class="main-content-container container-fluid px-4">
                     <!-- Page Header -->
-                    <div class="page-header row no-gutters py-4">
-                        <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
-                            <span class="text-uppercase page-subtitle">Overview</span>
-                            <h3 class="page-title">User Profile</h3>
-                        </div>
-                    </div>
+                    @yield('page_header')
                     <!-- End Page Header -->
                     <!-- Default Light Table -->
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <div class="card card-small mb-4">
-                                <div class="card-header border-bottom">
-                                    <h6 class="m-0">Create New Teacher</h6>
-                                </div>
-                                <div class="card-body">
-                                    <button type="submit" class="btn btn-sm btn-primary">
-                                        <i class="material-icons mr-1">save</i>Teacher</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-8">
-                            <div class="card card-small mb-4">
-                                <div class="card-header border-bottom">
-                                    <h6 class="m-0">Account Details</h6>
-                                </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item p-3">
 
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                    @yield('content')
+
                     <!-- End Default Light Table -->
                 </div>
                 <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
-                    <span class="copyright ml-auto my-auto mr-2">Copyright © 2018
+                    <span class="copyright ml-auto my-auto mr-2">Copyright © 2020
                         <a href="https://designrevision.com" rel="nofollow">DesignRevision</a>
                     </span>
                 </footer>
@@ -107,11 +77,10 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
         integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
     <script src="https://unpkg.com/shards-ui@latest/dist/js/shards.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Sharrre/2.0.1/jquery.sharrre.min.js"></script>
     <script src="{{ url('assets/scripts/extras.1.1.0.min.js') }}"></script>
-    <script src="{{ url('assets/scripts/shards-dashboards.1.1.0.min.js') }}"></script>
+    @stack('scripts')
 </body>
 
 </html>
