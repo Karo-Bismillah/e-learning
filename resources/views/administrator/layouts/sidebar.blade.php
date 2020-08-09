4ddd13501b82080830074198e2c31a29bbb17d53
Djendra Priono <djendrapriono@hotmail.com>
2020-08-09 12:06:11 +0700

    sidebar admin

--- resources/views/administrator/layouts/sidebar.blade.php
+++ resources/views/administrator/layouts/sidebar.blade.php
@@ -1,18 +1,25 @@
-<ul class="nav flex-column">
+<ul class="nav nav--no-borders flex-column">
     <li class="nav-item">
-        <a class="nav-link " href="index.html">
-            <i class="material-icons">edit</i>
-            <span>Blog Dashboard</span>
+        <a class="nav-link " href="transaction-history.html">
+            <i class="material-icons">dashboard</i>
+            <span>Dashboard</span>
         </a>
     </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-          <i class="material-icons"></i>
-          <span>User Account</span>
+    <li class="nav-item dropdown
+    {{( request()->is('admin/teacher')
+    or
+     request()->is('admin/student'
+     )) ? 'active show' : ''
+    }}
+    ">
+        <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="true">
+            <i class="material-icons">person</i>
+            <span>Manage User</span>
         </a>
-        <div class="dropdown-menu dropdown-menu-small" x-placement="bottom-start" style="display: none; position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-5px, 52px, 0px);">
-          <a class="dropdown-item " href="user-profile.html">User Profile</a>
-
+        <div class="dropdown-menu dropdown-menu-small {{(request()->is('admin/teacher')) ? 'show' : ''}}">
+            <a class="dropdown-item {{(request()->is('admin/teacher')) ? 'active' : ''}}" href="{{route('indexTeacher')}}">Teachers</a>
+            <a class="dropdown-item " href="file-manager-cards.html">Students</a>
         </div>
-      </li>
+    </li>
+
 </ul>
