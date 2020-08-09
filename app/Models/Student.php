c42422adeb05ada7e3bfea4da7c0e4971fe50004
Djendra Priono <djendrapriono@hotmail.com>
2020-08-09 17:34:00 +0700

    Relasi belongsTo Classroom

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $table = 'students';
+    protected $fillable = [
+        'user_id', 'class_id', 'name', 'gender', 'dob', 'address', 'created_at', 'updated_at',
+    ];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
 }
